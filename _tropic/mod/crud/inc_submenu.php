75e271b24f3482094510807ba02a344554b0cfb3
ApeRunner <beto.sitios@gmail.com>
2013-06-25 16:30:41 -0500

    custom return

--- _tropic/mod/crud/inc_submenu.php
+++ _tropic/mod/crud/inc_submenu.php
@@ -1,14 +1,27 @@
 
 <div align="center">
 
-
+	<!-- Custom Return -->
+	
+	<?php
+		if ($Custom_Return == 1){
+		?>
+		<a class="<?=$BTN_CSS?>" href="<?=$Custom_URL?>">
+        <i class="icon-arrow-left-3"></i> <?=$Custom_Label?>
+        </a>
+		
+	
+	
 	<!-- Padre -->
-    <?php if ( strlen($Parent_Field)>0 ) { 
+    <?php } elseif ( strlen($Parent_Field)>0 ) { 
 	?>
         <a class="<?=$BTN_CSS?>" href="<?=$Parent_File?>?a=list">
         <i class="icon-arrow-left-3"></i> <?=$Parent_Label?>
         </a>
 	<?php } ?>
+    
+    
+    
 
 	<!-- Lista Elemento -->
     <a class="<?=$BTN_CSS?>" href="<?=$filename?>?a=list&<?=$VarsParent?>">
@@ -20,8 +33,7 @@
     <i class="icon-plus"></i> Crear  <?=$Label_Singular?> 
     </a>
   
-  
-  
+
 </div>
 
 
